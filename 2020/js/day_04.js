95b8933ce44718f228c58cf3be1819b1192bf8f8
Omeed Safi <omeed@safi.ms>
2021-11-24 17:18:07 -0600

    [2020][js] day 4 wip

---
+++ 2020/js/day_04.js
@@ -0,0 +1,49 @@
+
+Array.prototype.count = function (predicate) {
+    return this.filter(predicate).length
+}
+
+RegExp.prototype.matchAllAsObject = function (str) {
+    const matches = [...this[Symbol.matchAll](str)]
+    return matches.reduce((acc, match) => ({ ...acc, [match[1]]: match[2] }), {})
+}
+
+String.prototype.extractNumbers = function () {
+    return Number(this.replaceAll(/[^0-9]/g, ''))
+}
+
+const inRange = function (number, beginInclusive, endInclusive) {
+    return beginInclusive <= Number(number) && Number(number) <= endInclusive
+}
+
+const input = require('fs')
+    .readFileSync(process.stdin.fd)
+    .toString()
+    .split('\n\n');
+
+
+const extractRegExp = /([a-z]{3}):([^\s]+)/g
+const passportData = input.map(record => extractRegExp.matchAllAsObject(record))
+
+const part1Result = passportData.count(({ ecl, pid, eyr, hcl, byr, iyr, hgt }) => ecl && pid && eyr && hcl && byr && iyr && hgt)
+
+const part2Result = passportData.count(({ ecl, pid, eyr, hcl, byr, iyr, hgt }) => {
+    const byrValid = inRange(byr, 1920, 2002)
+    const iyrValid = inRange(iyr, 2010, 2020)
+    const eyrValid = inRange(eyr, 2020, 2030)
+
+    const hgtValid = hgt && ((hgt.includes('cm') && inRange(hgt.extractNumbers(), 150, 193)) || (hgt.includes('in') && inRange(hgt.extractNumbers(), 59, 76)))
+
+    const eclValid = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(ecl)
+
+    const hclValid = /#[0-9a-f]{6}/.test(hcl)
+
+    const pidValid = /[0-9]{9}/.test(pid)
+
+    return byrValid && iyrValid && eyrValid && hgtValid && eclValid && hclValid && pidValid
+})
+
+console.log(part1Result)
+console.log(part2Result)
+
+// 138 - wrong, too high

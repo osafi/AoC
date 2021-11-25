bce43fdf32aa91e20fd16effe58df095ade5f621
Omeed Safi <omeed@safi.ms>
2021-11-25 12:09:55 -0600

    [2020][js] day 5 part 2

--- 2020/js/day_05.js
+++ 2020/js/day_05.js
@@ -3,10 +3,19 @@ const input = require('fs')
     .toString()
     .split('\n');
 
-const mapping = { 'F': '0', 'B': '1', 'L': '0', 'R': '1' };
+const mapping = {'F': '0', 'B': '1', 'L': '0', 'R': '1'};
 
-const result = input
-    .map(r => r.replace(/[FBLR]/g, m => mapping[m]))
-    .map(b => parseInt(b, 2))
+const seats =
+    input.map(r => r.replace(/[FBLR]/g, m => mapping[m]))
+        .map(b => parseInt(b, 2))
+        .sort((a, b) => a - b)
 
-console.log(Math.max(...result))
+const part1Result = seats[seats.length - 1]
+console.log(part1Result)
+
+for (let i = 0; i < seats.length; i++) {
+    if (seats[i] + 1 !== seats[i + 1]) {
+        console.log(seats[i] + 1)
+        break
+    }
+}

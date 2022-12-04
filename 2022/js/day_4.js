0d2197adf7565d1fbb19ef255076ca4e8259910b
Omeed Safi <omeed@safi.ms>
2022-12-04 16:47:15 -0600

    [2022][js] day 4 - updated with helper to do recursive line splitting

--- 2022/js/day_4.js
+++ 2022/js/day_4.js
@@ -2,21 +2,28 @@ Array.prototype.count = function (predicate) {
   return this.filter(predicate).length;
 }
 Array.prototype.toNum = function () {
-  return this.map(i => Number(i));
+  return this.map(i => i.constructor === Array ? i.toNum() : Number(i));
+}
+String.prototype.recursiveSplit = function (...delimiters) {
+  const [delimiter, ...rest] = delimiters;
+  if (typeof delimiter === 'string') {
+    return this.split(delimiter).map(s => s.recursiveSplit(...rest));
+  }
+  return this;
 }
 
 function part1(input) {
   return input
-    .split("\n")
-    .map(l => l.match(/(\d*)-(\d*),(\d*)-(\d*)/).slice(1, 5).toNum())
-    .count(([s1, e1, s2, e2]) => (s1 <= s2 && e1 >= e2) || (s2 <= s1 && e2 >= e1))
+    .recursiveSplit("\n", ",", "-")
+    .toNum()
+    .count(([[s1, e1], [s2, e2]]) => (s1 <= s2 && e1 >= e2) || (s2 <= s1 && e2 >= e1))
 }
 
 function part2(input) {
   return input
-    .split("\n")
-    .map(l => l.match(/(\d*)-(\d*),(\d*)-(\d*)/).slice(1, 5).toNum())
-    .count(([s1, e1, s2, e2]) => (s1 >= s2 && s1 <= e2) || (s2 >= s1 && s2 <= e1))
+    .recursiveSplit("\n", ",", "-")
+    .toNum()
+    .count(([[s1, e1], [s2, e2]]) => (s1 >= s2 && s1 <= e2) || (s2 >= s1 && s2 <= e1))
 }
 
 module.exports = {part1, part2};
